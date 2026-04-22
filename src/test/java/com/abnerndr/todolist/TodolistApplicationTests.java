ba8927e5fb0cd26e864ad0ee3a7dcb1cf089be02
Abner A Ananias <abnerndr.dev@gmail.com>
2026-04-22 13:55:52 -0300

    publish

---
+++ src/test/java/com/abnerndr/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abnerndr.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
